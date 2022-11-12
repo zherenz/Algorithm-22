e46173619803ba2b5be0502139c351a946863e10
Zheren Zhu <83292144+zherenz@users.noreply.github.com>
2022-11-12 14:56:14 -0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -43,23 +43,16 @@ class QuickSort:
 # merge sort O(nlogn)
 class MergeSort:        
     def mergesort(self, seq):
-        """归并排序"""
         if len(seq) <= 1:
             return seq
-        mid = len(seq) / 2  # 将列表分成更小的两个列表
-        # 分别对左右两个列表进行处理，分别返回两个排序好的列表
+        mid = len(seq) // 2
         left = self.mergesort(seq[:mid])
         right = self.mergesort(seq[mid:])
-        # 对排序好的两个列表合并，产生一个新的排序好的列表
         return self.merge(left, right)
 
     def merge(self, left, right):
-        """合并两个已排序好的列表，产生一个新的已排序好的列表"""
-        result = []  # 新的已排序好的列表
-        i = 0  # 下标
-        j = 0
-        # 对两个列表中的元素 两两对比。
-        # 将最小的元素，放到result中，并对当前列表下标加1
+        result = []
+        i, j = 0, 0
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
                 result.append(left[i])
